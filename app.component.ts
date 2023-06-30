b683398a3f69c8eaa20d9669eee576aee38a15df
Abril Gaia <abriltaurina23@gmail.com>
2023-06-30 16:58:25 -0300

    1

---
+++ app.component.ts
@@ -0,0 +1,175 @@
+<nav>
+    <div class="container-fluid">
+        
+        <div class="row" >
+             <div class="col"></div>
+             <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="file:///D:/Trabajo%20final-oficios/index.html#">Inicio</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                      <a class="nav-link active" aria-current="page" href="file:///D:/Trabajo%20final-oficios/contacto.html">Contactos</a>
+                      <a class="nav-link" href="file:///D:/Trabajo%20final-oficios/rejistrarce.html">Registrarse</a>
+                      <a class="nav-link" href=""></a>
+                      <a class="nav-link disabled"></a>
+                    </div>
+                  </div>
+                </div>
+              </nav>
+            </nav>
+ <main>
+    <header>
+              <div class="col"><img src="./img/iconoprincipal.png" alt="Icono" width="150" title="Icono"></div>
+       
+            <center><h1>Este sitio web fue creado para todas las personas en busca de cursos/capacitaciones de Villa Dolores</h1></center>
+            <h2>
+                "No hay ningun secreto para el éxito <br>
+                es el resultado de la preparación, <br>
+                el trabajo y aprender del fracaso" <br>
+                                     (Colin Powell) <br>
+            </h2>
+        
+    </div>
+</header>
+        <section>
+            <article>
+        <div>
+            <table>
+                <tr>
+                    <th> Institutos </th>
+                    <th>Cursos/Capacitaciones</th>
+                </tr>
+                <tr>
+                    <td><h6>ICEC. Instituto de capacitacion y estudios continuos</h6></td>
+                    <td> <a href="file:///D:/Trabajo%20final-oficios/pedicuriapodoesteticaICEC.html" class="link-danger">Pedicuría y Podo estética</a> </td>
+                </tr>
+                <tr>
+                    <td> </td>
+                    <td> <a href="file:///D:/Trabajo%20final-oficios/manicuriasemipermanenteICEC.html" class="link-danger">Taller de Manicuría y Semipermanente</a> </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td> <a href="file:///D:/Trabajo%20final-oficios/Ealisados_tracapilaresICEC.html" class="link-danger">Especialista en Alisados y Tratamientos Capilares</a></td>
+                </tr> 
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/acompañanteterapueticoICEC.html" class="link-danger">Acompañante Terapéutico</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/secretariadolaboralCEC.html" class="link-danger">Secretariado Laboral</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/peluqueria_profesionalICEC.html" class="link-danger">Peluquería Profesional</a></td>
+
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/masajista_profesionalICEC.html" class="link-danger">Masajista Profesional</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/auxiliarfarmaceuticoICEC.html" class="link-danger">Auxiliar Farmacéutico</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/paramedicoICEC.html" class="link-danger">Curso de Paramédico</a></td>
+                </tr>
+                
+                <tr>
+                    <td>
+                        <div>
+                        <button type="button" onclick="window.alert('Este botón los llevará a la direccion actual del Instituto ICEC en el mapa de Villa Dolores. Precione aceptar si esta de acuerdo con esta condición.')"><a href="https://www.google.com/search?tbs=lf:1,lf_ui:2&tbm=lcl&q=instituto+icec+villa+dolores&rflfq=1&num=10&rllag=-31948088,-65193882,618&ved=2ahUKEwi3jtvm3ML_AhV6qpUCHXv7AFEQtgN6BAgWEAI&safe=active&ssui=on#rlfi=hd:;si:8839464820569531601,l,ChxpbnN0aXR1dG8gaWNlYyB2aWxsYSBkb2xvcmVzSMnS796cq4CACFoqEAAQARgAGAEYAhgDIhxpbnN0aXR1dG8gaWNlYyB2aWxsYSBkb2xvcmVzkgEXZWR1Y2F0aW9uYWxfaW5zdGl0dXRpb26qAVkQASoSIg5pbnN0aXR1dG8gaWNlYygOMh8QASIbxs3dKvD7J8pKSJjw8uJTsHZzeTDkOr2onGruMiAQAiIcaW5zdGl0dXRvIGljZWMgdmlsbGEgZG9sb3Jlcw;mv:[[-31.945850399999998,-65.1870307],[-31.950326799999996,-65.20073409999999]]" class="link-danger" > Dirección del Intituto ICEC: Felipe Erdman 231, Villa Dolores, Córdoba</a></button>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Instituto Gastronómico Internacional</th>
+                    <th>Curso/Capacitaciones</th>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/InstitutoGChef.html" class="link-warning">Chef Profesional</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/InstitutoGPastelero.html" class="link-warning">Pastelero Profesional</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/InstitutoGPanadero.html" class="link-warning">Panadero Profesional</a></td>
+                </tr>
+                <tr>
+                    <td>
+                        <div>
+                        <button type="button" onclick="window.alert('Este botón los llevará a la direccion actual del Instituto Gastronómico Internacional en el mapa de Villa Dolores. Precione aceptar si esta de acuerdo con esta condición.')"><a href="https://www.google.com/maps/uv?pb=!1s0x942cd49fc34a4413%3A0xa73a42a7df06b77b!3m1!7e115!4s%2Fmaps%2Fplace%2Finstituto%2Bgastronomico%2Binternacional%2Bde%2Bvilla%2Bdolores%2F%40-31.9431032%2C-65.1898654%2C3a%2C75y%2C234.7h%2C90t%2Fdata%3D*213m4*211e1*213m2*211sLiUNcGbQNoVeDkeGpnkl3g*212e0*214m2*213m1*211s0x942cd49fc34a4413%3A0xa73a42a7df06b77b%3Fsa%3DX!5sinstituto%20gastronomico%20internacional%20de%20villa%20dolores%20-%20Buscar%20con%20Google!15sCgIgAQ&imagekey=!1e2!2sLiUNcGbQNoVeDkeGpnkl3g&hl=es&sa=X&ved=2ahUKEwiC18KN2sL_AhUxl5UCHQBvDakQpx96BAg4EA0" class="link-warning">Dirección del Instituto Gastronómico (IGI): 42 25 de Mayo Villa Dolores, Córdoba</a></button>
+                        </div>
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Instituto Celsius</th>
+                    <th>Cursos/Capacitaciones</th>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/reparadorcelularesCelsius.html" class="link-info">Reparador de Celulares</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/facturacionmedicacelsius.html" class="link-info">Facturación Médica</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/electricidadomesticaCelsius.html" class="link-info">Electricidad Domiciliaria</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/programadorCelsius.html" class="link-info">Programador</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/mecanicomotoscelsius.html" class="link-info">Mecánico de Motos</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/InsRepde_AireAcondicionado.html" class="link-info">Instalación y Reparación de Aire Acondicionado</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="file:///D:/Trabajo%20final-oficios/termotanquesolarCelsius.html" class="link-info">Curso de Instalador de Termotanques Solares.</a></td>
+                </tr>
+                <tr>
+                    <td> 
+                        <div>
+                        <button type="button" onclick="window.alert('Este botón los llevará a la direccion actual del Instituto Celsius en el mapa de Villa Dolores. Precione aceptar si esta de acuerdo con esta condición.')"><a href="https://www.google.com/maps/place/Instituto+Celsius+(Villa+Dolores)/@-31.9450287,-65.1878415,15z/data=!4m6!3m5!1s0x942cd5c89f693f25:0x2b2330e07b94ce0c!8m2!3d-31.9450287!4d-65.1878415!16s%2Fg%2F11k676d4nf?entry=ttu" class="link-info">Dirección del Instituto Celsius: Italia 13, X5885 Villa Dolores, Córdoba</a></button>
+                        </div>
+                    </td>
+                    <td></td>
+                </tr>
+            </table>
+
+        </div>
+    </article>
+    </section>
+    </main>
+    <footer>
+        <div class="row">
+            <div class="col">
+                <i class="fa fa-copyright"aria-hidden="true">Creado en 2023, por las estudiantes Noel Fernández, Abril Gaia, Milagros Suarez y Morena Moran.</i>
+                <br>
+                 <i>Dirrección:Vélez Sarsfield, Nro 196, de la ciudad de San Pedro Provincia de Córdoba.</i>
+                 <br>
+                 <i>Al usar este sitio, usted acepta nuestros términos de uso.</i>
+                 <br>
+                 <i>Fecha de Lanzamineto: 29/06/2023</i>
+                 <br>
+                 <i>Trabajo Escolar</i>
+            </div>
+    
+          </div>
+    </footer>
